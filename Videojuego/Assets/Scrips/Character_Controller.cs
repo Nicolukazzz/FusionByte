4f21fd56cca9884ac44e859e4920c96e6ea9a243
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2024-08-22 15:08:30 -0500

    Movimiento1.0

---
+++ Videojuego/Assets/Scrips/Character_Controller.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    public float Speed = 10f;
+    public float JumpForce = 12f;
+    public float MaxHoldTime = 0.5f; // Tiempo máximo que se puede mantener presionada la tecla de salto
+    public LayerMask capaFloor;
+    public int MaxJumps = 2;
+    public float fallMultiplier = 2.5f; // Factor que incrementa la velocidad de caída
+    public float lowJumpMultiplier = 2f; // Factor que desacelera la caída cuando se suelta la tecla
+
+    private bool LookRight = true;
+    private int RestJumps;
+    private new Rigidbody2D rigidbody;
+    private BoxCollider2D boxCollider;
+
+    private bool isJumping;
+    private float jumpTimeCounter;
+
+    public void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        Movement();
+        Jump();
+        ApplyGravityModifiers();
+    }
+
+    bool InFloor()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, capaFloor);
+        return raycastHit.collider != null;
+    }
+
+    void Jump()
+    {
+        // Si el personaje está en el suelo, restablece el número de saltos disponibles
+        if (InFloor())
+        {
+            RestJumps = MaxJumps;
+        }
+
+        // Inicia el salto
+        if (Input.GetKeyDown(KeyCode.Space) && RestJumps > 0)
+        {
+            isJumping = true;
+            jumpTimeCounter = MaxHoldTime;
+            RestJumps--;
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpForce);
+        }
+
+        // Mantén el salto mientras se mantenga presionada la tecla
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, JumpForce);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        // Finaliza el salto al soltar la tecla
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+    }
+
+    void ApplyGravityModifiers()
+    {
+        if (rigidbody.velocity.y < 0)
+        {
+            // Aplica un multiplicador para acelerar la caída
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            // Aplica un multiplicador para suavizar la caída si se suelta la tecla de salto
+            rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void Movement()
+    {
+        // Movimiento del personaje
+        float inputMovement = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(inputMovement * Speed, rigidbody.velocity.y);
+        Orientation(inputMovement);
+    }
+
+    void Orientation(float inputMovement)
+    {
+        // Orientación del personaje
+        if ((LookRight && inputMovement < 0) || (!LookRight && inputMovement > 0))
+        {
+            LookRight = !LookRight;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+}
