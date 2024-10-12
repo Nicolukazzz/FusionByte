9299515e9ff6b61e25c759f3a4d1905dcaecc7d5
Solaquin <145218692+Solaquin@users.noreply.github.com>
2024-10-12 10:36:29 -0500

    added scenes and scenes manager a "Videojuego"
    se agregó las escenas de opciones, menu, pantalla inicial, niveles a la carpeta del juego principal

---
+++ Videojuego/Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void optionsButton()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+
+    public void exitButton()
+    {
+        Application.Quit();
+    }
+
+    public void startButton() 
+    {
+        SceneManager.LoadScene(4, LoadSceneMode.Single);
+    }
+
+    public void levelsButton()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    public void backButton()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+}
