2b02b3f30a055a160f855b11b7bc634c3dba0425
Solaquin <145218692+Solaquin@users.noreply.github.com>
2024-10-12 09:52:22 -0500

    daño fix

---
+++ Videojuego/Assets/Scripts/Enemy_controller.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemycontroller : MonoBehaviour
+{
+    [SerializeField] private float vida = 100;
+    private Animator animator;
+    private bool isDead;
+    private bool recibiendoDano;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TomarDano(float Dano)
+    {
+
+        do
+        {
+            if (vida < Dano)
+            {
+                vida = 0;
+            }
+            else
+            {
+                vida -= Dano;
+            }
+            recibiendoDano = true;
+        } while (vida > 0 && !recibiendoDano && !isDead);
+
+        recibiendoDano = false;
+
+        if (vida <= 0)
+        {
+            Muerte();
+        }
+    }
+
+    private void Muerte()
+    {
+        isDead = true;
+        animator.SetTrigger("Muerte");
+    }
+
+    public bool getIsDead()
+    {
+        return isDead;
+    }
+}
