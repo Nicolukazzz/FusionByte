cab00d0aa9fee93b0df5fa2285f2743489d3bd00
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2024-10-20 17:29:29 -0500

    Animacion fondos
    Se animaron todos los fondos de todos los niveles

---
+++ Videojuego/Assets/Scripts/MovimientoParticulasAbajo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoParticulasAbajo : MonoBehaviour
+{
+  
+    [SerializeField] private Vector2 velocidadMovimiento;
+    private Vector2 offset;
+    private Material material;
+    private Rigidbody2D jugadorRB;
+
+    private void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+
+    private void Update()
+    {
+        offset = velocidadMovimiento * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+
+}
