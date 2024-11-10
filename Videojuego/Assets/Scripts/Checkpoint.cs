02a0b83007be620d9bc2e914d9e4db85e014db5b
Solaquin <145218692+Solaquin@users.noreply.github.com>
2024-11-10 00:54:32 -0500

    add checkpoint animation

--- Videojuego/Assets/Scripts/Checkpoint.cs
+++ Videojuego/Assets/Scripts/Checkpoint.cs
@@ -9,8 +9,13 @@ public class Checkpoint : MonoBehaviour
     [SerializeField] private bool isEndCheckpoint;
     [SerializeField] private Scene_Manager sceneManager;
     [SerializeField] private AudioClip CheckpointSound;
+    private Animator animator;
     //[SerializeField] private int nextLevel;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,6 +25,7 @@ public class Checkpoint : MonoBehaviour
             if (player != null)
             {
                 player.SetCheckpoint(transform.position);
+                animator.SetBool("IsCheckpointActive", true);
                 ControladorSonidos.Instance.EjecutarSonido(CheckpointSound);
             }
 
